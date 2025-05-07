c10e2c9378355922047dc4524b54e0e3b0a5c35a
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-05-08 02:48:38 +0300

    create RabbitMQConfig

---
+++ eda-rabbitmq-order-service/src/main/java/com/demo/edarabbitmqorderservice/config/RabbitMQConfig.java
@@ -0,0 +1,56 @@
+package com.demo.edarabbitmqorderservice.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.order.name}")
+    public String orderQueue;
+
+    @Value("${rabbitmq.exchange.name}")
+    public String exchangeName;
+
+    @Value("${rabbitmq.order.routing.key}")
+    public String orderRoutingKey;
+
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(orderQueue);
+    }
+
+    @Bean
+    public TopicExchange orderExchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding orderBinding() {
+        return BindingBuilder
+                .bind(orderQueue())
+                .to(orderExchange())
+                .with(orderRoutingKey);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+}
