7d6d85b6206f2eab5566e25b0c8ee23cbc39e6c8
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-05-08 03:01:34 +0300

    create OrderEventProducer

---
+++ eda-rabbitmq-order-service/src/main/java/com/demo/edarabbitmqorderservice/rabbitmq/OrderEventProducer.java
@@ -0,0 +1,32 @@
+package com.demo.edarabbitmqorderservice.rabbitmq;
+
+import com.demo.edarabbitmqorderservice.model.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderEventProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderEventProducer.class);
+
+    @Value("${rabbitmq.order.exchange.name}")
+    private String orderExchangeName;
+
+    @Value("${rabbitmq.order.routing.key}")
+    private String orderRoutingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public OrderEventProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void produceOrderEvent(OrderEvent orderEvent) {
+        LOGGER.info(String.format("Order Event produced : %s", orderEvent));
+        rabbitTemplate.convertAndSend(orderExchangeName, orderRoutingKey, orderEvent);
+    }
+
+}
